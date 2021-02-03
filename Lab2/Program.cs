a802de4b69d7b7226d9983adf4b52ecd94d4906d
Artiom Ciobanu <aciobanumain@gmail.com>
2021-02-03 22:38:19 +0200

    Added Chord method

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -7,16 +7,25 @@ namespace Lab2
         private static double F(double x)
             => 1.5 - 0.4 * Math.Sqrt(Math.Pow(x, 3)) - 0.5 * Math.Log(x);
 
+        private static double SecondDerivative(double x)
+            => 0.5 * (-0.3 / Math.Sqrt(x) - 1 / (x * x));
+
         private static void Main()
         {
             double a = 2;
             double b = 2.5;
 
-            var iterationsAmount = (int) Math.Ceiling(Math.Log2(Math.Abs(b - a) / Math.Pow(10000, -5)));
+            var precision = Math.Pow(10000, -5);
+
+            //Метод Деления отрезка пополам
+            var iterationsAmount = (int) Math.Ceiling(Math.Log2(Math.Abs(b - a) / precision));
 
             Console.WriteLine(F(a) * F(b) > 0 ?
                 $"На отрезке[{a}, {b}] нет корня." :
-                $"Примерный ответ: {Dichotomy(a, b, iterationsAmount)}");
+                $"Примерный ответ методом Дихотомии: {Dichotomy(a, b, iterationsAmount)}");
+
+            Console.WriteLine(
+                $"Метод Хорд: {ChordMethod(a, b, precision)}");
         }
 
         private static double Dichotomy(double a, double b, int iterationsAmount)
@@ -37,6 +46,26 @@ namespace Lab2
             }
 
             return a > b ? b : a;
+        }
+
+        private static double ChordMethod(double a, double b, double precision)
+        {
+            if (SecondDerivative(a) > 0)
+            {
+                var c = a;
+                a = b;
+                b = c;
+            }
+
+            double difference = 0;
+            do
+            {
+                difference = F(a) * (b - a) / (F(b) - F(a));
+
+                a += difference;
+            } while (difference > precision);
+
+            return a;
         }
     }
 }
