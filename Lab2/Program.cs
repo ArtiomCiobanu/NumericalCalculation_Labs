539204a86414cdf009d9b7f9b54c3574665ce670
Artiom Ciobanu <aciobanumain@gmail.com>
2021-02-03 23:39:58 +0200

    Added Secant method

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -7,6 +7,9 @@ namespace Lab2
         private static double F(double x)
             => 1.5 - 0.4 * Math.Sqrt(Math.Pow(x, 3)) - 0.5 * Math.Log(x);
 
+        private static double FirstDerivative(double x)
+            => (-0.6 * x * Math.Sqrt(x) - 0.5) / x;
+
         private static double SecondDerivative(double x)
             => 0.5 * (-0.3 / Math.Sqrt(x) - 1 / (x * x));
 
@@ -24,8 +27,9 @@ namespace Lab2
                 $"На отрезке[{a}, {b}] нет корня." :
                 $"Примерный ответ методом Дихотомии: {Dichotomy(a, b, iterationsAmount)}");
 
-            Console.WriteLine(
-                $"Метод Хорд: {ChordMethod(a, b, precision)}");
+            Console.WriteLine($"Методом Хорд: {ChordMethod(a, b, precision)}");
+            Console.WriteLine($"Методом Ньютона(Касательных): {NewtonMethod(a, b, precision)}");
+            Console.WriteLine($"Методом Секущих: {SecantMethod(2.5, 3, precision)}");
         }
 
         private static double Dichotomy(double a, double b, int iterationsAmount)
@@ -50,7 +54,7 @@ namespace Lab2
 
         private static double ChordMethod(double a, double b, double precision)
         {
-            if (SecondDerivative(a) > 0)
+            if (F(a) * SecondDerivative(a) < 0)
             {
                 var c = a;
                 a = b;
@@ -62,7 +66,45 @@ namespace Lab2
             {
                 difference = F(a) * (b - a) / (F(b) - F(a));
 
+                a -= difference;
+            } while (difference > precision);
+
+            return a;
+        }
+
+        private static double NewtonMethod(double a, double b, double precision)
+        {
+            if (F(a) * SecondDerivative(a) > 0)
+            {
+                var c = a;
+                a = b;
+                b = c;
+            }
+
+            double difference = 0;
+            do
+            {
+                difference = F(a) / FirstDerivative(a);
                 a += difference;
+            } while (difference > precision);
+
+            return a;
+        }
+
+        private static double SecantMethod(double a, double b, double precision)
+        {
+            if (b > a)
+            {
+                var c = a;
+                a = b;
+                b = c;
+            }
+
+            double difference = 0;
+            do
+            {
+                difference = F(a) * (b - a) / (F(b) - F(a));
+                a -= difference;
             } while (difference > precision);
 
             return a;
